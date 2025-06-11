be114b9a0c589c40b0334065c49f4a70c02cd134
Carrie E. Holt <31521619+carrholt@users.noreply.github.com>
2025-06-11 18:48:22 -0400

    initial files

---
+++ sso_query/query.py
@@ -0,0 +1,8 @@
+
+
+
+def make_query(q_cutoff=1.3, e_cutoff=1.0, a_cutoff=10.0):
+    query = f"""SELECT foo FROM bar 
+        WHERE q < {q_cutoff} AND e < {e_cutoff} AND a < {a_cutoff};"""
+
+    return query
