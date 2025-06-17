76f60761e2c04f896e8b15e64b81940c00238cb8
amineeva <amineeva@olin.edu>
2025-06-17 17:41:54 +0000

    first query made and tests passed

--- sso_query/query.py
+++ sso_query/query.py
@@ -2,9 +2,10 @@
 # pretty sure we're using the MPCORB 10 year data table, going with that first
 
 def make_query(q_cutoff=1.3, e_cutoff=1.0, a_cutoff=10.0):
-    query = f"""e, q, a foo FROM dp03_catalogs_10yr.MPCORB 
-        WHERE q < {q_cutoff} AND e < {e_cutoff} AND a < {a_cutoff};"""
+    query = f"""e, q, a FROM dp03_catalogs_10yr.MPCORB as mpc "
+        WHERE mpc.q < {q_cutoff} AND mpc.e < {e_cutoff} AND mpc.a < {a_cutoff};"""
 
     return query
 
     # query takes the e, q, a columns from the MPCORB 10 year catalog, with the cutoff values
+
--- tests/test_query.py
+++ tests/test_query.py
@@ -2,14 +2,17 @@ from sso_query.query import make_query
 
 class TestQuery:
     def test_defaults(self):
-        expected_query = "foo"
+        expected_query = f"""e, q, a FROM dp03_catalogs_10yr.MPCORB as mpc "
+        WHERE mpc.q < 1.3 AND mpc.e < 1.0 AND mpc.a < 10.0;"""
+
 
         query = make_query()
 
         assert expected_query == query
 
     def test_neos(self):
-        expected_query = "foo"
+        expected_query = f"""e, q, a FROM dp03_catalogs_10yr.MPCORB as mpc "
+        WHERE mpc.q < 1.3 AND mpc.e < 1.0 AND mpc.a < 10.0;"""
 
         query = make_query(1.3)
 
