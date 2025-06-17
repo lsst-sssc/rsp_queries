f810ce2cdd67f5dd1303243d408213babfa219a1
amineeva <amineeva@olin.edu>
2025-06-17 16:10:58 +0000

    first query - MPCORB 10 year dataset

--- sso_query/query.py
+++ sso_query/query.py
@@ -1,11 +1,10 @@
 
-
+# pretty sure we're using the MPCORB 10 year data table, going with that first
 
 def make_query(q_cutoff=1.3, e_cutoff=1.0, a_cutoff=10.0):
-    query = f"""SELECT foo FROM bar 
+    query = f"""e, q, a foo FROM dp03_catalogs_10yr.MPCORB 
         WHERE q < {q_cutoff} AND e < {e_cutoff} AND a < {a_cutoff};"""
 
     return query
 
-    # testing that the clone worked?
-    # try 2
+    # query takes the e, q, a columns from the MPCORB 10 year catalog, with the cutoff values
