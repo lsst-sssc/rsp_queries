e201425e2a5fb0c5ea4fee289e6b4737469aa146
amineeva <amineeva@olin.edu>
2025-06-18 17:22:03 +0000

    Made query prefix its own variable up at the top.

--- sso_query/query.py
+++ sso_query/query.py
@@ -1,60 +1,54 @@
 
 # MPCORB 10-year data table
-# ordered in terms of specificity - narrow cases first, broader cases last
+# ordered in terms of specificity - narrow cases first
 
 def make_query(q_cutoff = 1.0, e_cutoff = 0.5, a_cutoff = 5.0, a_cutoff_min = 5.0): #q_cutoff=1.3, e_cutoff=1.0, a_cutoff=10.0, a_cutoffmin=5.5
+    query_start = f"""SELECT a, q, e FROM dp03_catalogs_10yr.MPCORB as mpc
+            WHERE """
+    
     # centaurs
     if(a_cutoff >= 30.1 and a_cutoff_min <= 5.5):
-        query = f"""SELECT a FROM dp03_catalogs_10yr.MPCORB as mpc
-            WHERE mpc.a > {a_cutoff_min} AND mpc.a < {a_cutoff};"""
+        query = query_start + f"mpc.a > {a_cutoff_min} AND mpc.a < {a_cutoff};"
         return query
         # 5.5 au < a < 30.1 au
     
     # mbas
     if(a_cutoff >= 3.2 and a_cutoff_min <= 2.0 and q_cutoff >= 1.66):
-        query = f"""SELECT a, q FROM dp03_catalogs_10yr.MPCORB as mpc
-            WHERE mpc.a > {a_cutoff_min} AND mpc.a < {a_cutoff} AND mpc.q > {q_cutoff};"""
+        query = query_start + f"mpc.a > {a_cutoff_min} AND mpc.a < {a_cutoff} AND mpc.q > {q_cutoff};"
         return query
         # 2.0 au < a < 3.2 au, q > 1.66 au
 
     # lpcs
     if(a_cutoff_min >= 50):
-        query = f"""SELECT a FROM dp03_catalogs_10yr.MPCORB as mpc
-            WHERE mpc.a > {a_cutoff_min};"""
+        query = query_start + f"mpc.a > {a_cutoff_min};"
         return query
         # a > 50 au
 
     # tnos
     if(a_cutoff <= 50 and a_cutoff_min >= 30.1):
-        query = f"""SELECT a FROM dp03_catalogs_10yr.MPCORB as mpc
-            WHERE mpc.a > {a_cutoff_min} AND mpc.a < {a_cutoff};"""
+        query = query_start + f"mpc.a > {a_cutoff_min} AND mpc.a < {a_cutoff};"
         return query
         # 30.1 au < a < 50 au
 
 
     # jtrojans
     if(a_cutoff_min >= 4.8 and a_cutoff <= 5.4 and e_cutoff <= 0.3):
-        query = f"""SELECT a, e FROM dp03_catalogs_10yr.MPCORB as mpc
-            WHERE mpc.a > {a_cutoff_min} AND mpc.a < {a_cutoff} AND mpc.e < {e_cutoff};"""
+        query = query_start + f"mpc.a > {a_cutoff_min} AND mpc.a < {a_cutoff} AND mpc.e < {e_cutoff};"
         return query
         # 4.8 au < a < 5.4 au, e < 0.3
 
 
     # ntrojans
     if(a_cutoff_min >= 29.8 and a_cutoff <= 30.4):
-        query = f"""SELECT a FROM dp03_catalogs_10yr.MPCORB as mpc
-            WHERE mpc.a > {a_cutoff_min} AND mpc.a < {a_cutoff};"""
+        query = query_start + f"mpc.a > {a_cutoff_min} AND mpc.a < {a_cutoff};"
         return query
         # 29.8 au < a < 30.4 au
         
 
         # neos
     if(q_cutoff <= 1.3 and a_cutoff >= 4 and e_cutoff <= 1):
-        query = f"""SELECT e, q, a FROM dp03_catalogs_10yr.MPCORB as mpc
-            WHERE mpc.q < {q_cutoff} AND mpc.e < {e_cutoff} AND mpc.a > {a_cutoff};"""
+        query = query_start + f"mpc.q < {q_cutoff} AND mpc.e < {e_cutoff} AND mpc.a > {a_cutoff};"
         return query
         # q < 1.3, e < 1, a > 4.0
 
-
-    return query
 
--- tests/test_query.py
+++ tests/test_query.py
@@ -11,7 +11,7 @@ class TestQuery:
     #     assert expected_query == query
 
     def test_neos(self):
-        expected_query = f"""SELECT e, q, a FROM dp03_catalogs_10yr.MPCORB as mpc
+        expected_query = f"""SELECT a, q, e FROM dp03_catalogs_10yr.MPCORB as mpc
             WHERE mpc.q < 1.3 AND mpc.e < 1.0 AND mpc.a > 4.0;"""
 
         query = make_query(q_cutoff=1.3, a_cutoff=4.0, e_cutoff = 1.0)
@@ -21,7 +21,7 @@ class TestQuery:
         # q < 1.3 au (a > 4, e < 1)
 
     def test_centaurs(self):
-        expected_query = f"""SELECT a FROM dp03_catalogs_10yr.MPCORB as mpc
+        expected_query = f"""SELECT a, q, e FROM dp03_catalogs_10yr.MPCORB as mpc
             WHERE mpc.a > 5.5 AND mpc.a < 30.1;"""
 
         query = make_query(a_cutoff = 30.1, a_cutoff_min = 5.5)
@@ -31,7 +31,7 @@ class TestQuery:
         # 5.5 au < a < 30.1 au
 
     def test_mbas(self):
-        expected_query = f"""SELECT a, q FROM dp03_catalogs_10yr.MPCORB as mpc
+        expected_query = f"""SELECT a, q, e FROM dp03_catalogs_10yr.MPCORB as mpc
             WHERE mpc.a > 2.0 AND mpc.a < 3.2 AND mpc.q > 1.66;"""
 
         query = make_query(a_cutoff = 3.2, a_cutoff_min = 2.0, q_cutoff = 1.66)
@@ -41,7 +41,7 @@ class TestQuery:
         # 2.0 au < a < 3.2 au, q > 1.66 au
 
     def test_lpcs(self):
-        expected_query = f"""SELECT a FROM dp03_catalogs_10yr.MPCORB as mpc
+        expected_query = f"""SELECT a, q, e FROM dp03_catalogs_10yr.MPCORB as mpc
             WHERE mpc.a > 50;"""
 
         query = make_query(a_cutoff_min = 50)
@@ -51,7 +51,7 @@ class TestQuery:
         # a > 50 au
 
     def test_tnos(self):
-        expected_query = f"""SELECT a FROM dp03_catalogs_10yr.MPCORB as mpc
+        expected_query = f"""SELECT a, q, e FROM dp03_catalogs_10yr.MPCORB as mpc
             WHERE mpc.a > 30.1 AND mpc.a < 50;"""
 
         query = make_query(a_cutoff_min = 30.1, a_cutoff = 50)
@@ -61,7 +61,7 @@ class TestQuery:
         # 30.1 au < a < 50 au
 
     def test_jtrojans(self):
-        expected_query = f"""SELECT a, e FROM dp03_catalogs_10yr.MPCORB as mpc
+        expected_query = f"""SELECT a, q, e FROM dp03_catalogs_10yr.MPCORB as mpc
             WHERE mpc.a > 4.8 AND mpc.a < 5.4 AND mpc.e < 0.3;"""
 
         query = make_query(a_cutoff_min = 4.8, a_cutoff = 5.4, e_cutoff = 0.3)
@@ -71,7 +71,7 @@ class TestQuery:
         # 4.8 au < a < 5.4 au, e < 0.3
 
     def test_ntrojans(self):
-        expected_query = f"""SELECT a FROM dp03_catalogs_10yr.MPCORB as mpc
+        expected_query = f"""SELECT a, q, e FROM dp03_catalogs_10yr.MPCORB as mpc
             WHERE mpc.a > 29.8 AND mpc.a < 30.4;"""
 
         query = make_query(a_cutoff_min = 29.8, a_cutoff = 30.4)
