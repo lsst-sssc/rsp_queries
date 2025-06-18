41b418aa11129dada645d7dad975c48ae2046aa8
amineeva <amineeva@olin.edu>
2025-06-18 17:26:00 +0000

    Changed 'if' statements to 'elif' - tests are still passing

--- sso_query/query.py
+++ sso_query/query.py
@@ -13,40 +13,40 @@ def make_query(q_cutoff = 1.0, e_cutoff = 0.5, a_cutoff = 5.0, a_cutoff_min = 5.
         # 5.5 au < a < 30.1 au
     
     # mbas
-    if(a_cutoff >= 3.2 and a_cutoff_min <= 2.0 and q_cutoff >= 1.66):
+    elif(a_cutoff >= 3.2 and a_cutoff_min <= 2.0 and q_cutoff >= 1.66):
         query = query_start + f"mpc.a > {a_cutoff_min} AND mpc.a < {a_cutoff} AND mpc.q > {q_cutoff};"
         return query
         # 2.0 au < a < 3.2 au, q > 1.66 au
 
     # lpcs
-    if(a_cutoff_min >= 50):
+    elif(a_cutoff_min >= 50):
         query = query_start + f"mpc.a > {a_cutoff_min};"
         return query
         # a > 50 au
 
     # tnos
-    if(a_cutoff <= 50 and a_cutoff_min >= 30.1):
+    elif(a_cutoff <= 50 and a_cutoff_min >= 30.1):
         query = query_start + f"mpc.a > {a_cutoff_min} AND mpc.a < {a_cutoff};"
         return query
         # 30.1 au < a < 50 au
 
 
     # jtrojans
-    if(a_cutoff_min >= 4.8 and a_cutoff <= 5.4 and e_cutoff <= 0.3):
+    elif(a_cutoff_min >= 4.8 and a_cutoff <= 5.4 and e_cutoff <= 0.3):
         query = query_start + f"mpc.a > {a_cutoff_min} AND mpc.a < {a_cutoff} AND mpc.e < {e_cutoff};"
         return query
         # 4.8 au < a < 5.4 au, e < 0.3
 
 
     # ntrojans
-    if(a_cutoff_min >= 29.8 and a_cutoff <= 30.4):
+    elif(a_cutoff_min >= 29.8 and a_cutoff <= 30.4):
         query = query_start + f"mpc.a > {a_cutoff_min} AND mpc.a < {a_cutoff};"
         return query
         # 29.8 au < a < 30.4 au
         
 
         # neos
-    if(q_cutoff <= 1.3 and a_cutoff >= 4 and e_cutoff <= 1):
+    elif(q_cutoff <= 1.3 and a_cutoff >= 4 and e_cutoff <= 1):
         query = query_start + f"mpc.q < {q_cutoff} AND mpc.e < {e_cutoff} AND mpc.a > {a_cutoff};"
         return query
         # q < 1.3, e < 1, a > 4.0
