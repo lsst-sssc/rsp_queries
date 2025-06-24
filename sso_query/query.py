3ff4e85cd880c8fd1404ee1064094070629bdb97
norachasetatko <nc108@wellesley.edu>
2025-06-24 03:42:38 +0000

    first draft of queries and tests for all classes

--- sso_query/query.py
+++ sso_query/query.py
@@ -1,11 +1,41 @@
+def make_query(q_min=None, q_max=None, e_min=None, e_max=None, a_min=None, a_max=None):
+    start_query = f"""SELECT mpc.ssObjectId, mpc.mpcDesignation, mpc.e, mpc.q, mpc.incl FROM dp03_catalogs_10yr.MPCORB as mpc WHERE """
 
+    # LPCs: a >= 50.0
+    if (a_min is not None and a_min >=50.0):
+        query = start_query + f"mpc.q/(1.0-mpc.e) >= {a_min} ORDER by mpc.mpcDesignation"
 
+    # Centaurs: 5.5 < a < 30.1
+    if (a_min is not None and a_min >= 5.5 and
+        a_max is not None and a_max <= 30.1):
+        query = start_query + f"mpc.q/(1.0-mpc.e) >= {a_min} AND mpc.q/(1.0-mpc.e) <= {a_max} ORDER by mpc.mpcDesignation"
+    
+    # TNOs: 30.1 < a < 50
+    if (a_min is not None and a_min >= 30.1 and
+        a_max is not None and a_max <= 50.0):
+        query = start_query + f"mpc.q/(1.0-mpc.e) >= {a_min} AND mpc.q/(1.0-mpc.e) <= {a_max} ORDER by mpc.mpcDesignation" 
 
-def make_query(q_cutoff=1.3, e_cutoff=1.0, a_cutoff=10.0):
-    query = f"""SELECT foo FROM bar 
-        WHERE q < {q_cutoff} AND e < {e_cutoff} AND a < {a_cutoff};"""
+    # Neptunian Trojans: 29.8 < a < 30.4
+    if (a_min is not None and a_min >= 29.8 and
+        a_max is not None and a_max <= 30.4):
+        query = start_query + f"mpc.q/(1.0-mpc.e) >= {a_min} AND mpc.q/(1.0-mpc.e) <= {a_max} ORDER by mpc.mpcDesignation" 
 
+    # Jupiter Trojans: e < 0.3, 4.8 < a < 5.4
+    if (e_max is not None and e_max <= 0.3 and
+        a_min is not None and a_min >= 4.8 and
+        a_max is not None and a_max <= 5.4):
+        query = start_query + f"mpc.e <= {e_max} AND mpc.q/(1.0-mpc.e) >= {a_min} AND mpc.q/(1.0-mpc.e) <= {a_max} ORDER by mpc.mpcDesignation"
+    
+    # MBAs: q > 1.66, 2.0 < a < 3.2
+    if (q_min is not None and q_min >= 1.66 and 
+        a_min is not None and a_min >= 2.0 and 
+        a_max is not None and a_max <= 3.2):
+        query = start_query + f"mpc.q >= {q_min} AND mpc.q/(1.0-mpc.e) >= {a_min} AND mpc.q/(1.0-mpc.e) <= {a_max} ORDER by mpc.mpcDesignation"
+    
+    # NEOs: q < 1.3, e < 1.0, a > 4.0
+    elif (q_max is not None and q_max <= 1.3 and 
+          e_max is not None and e_max <= 1.0 and 
+          a_min is not None and a_min >= 4.0):
+        query = start_query + f"mpc.q <= {q_max} AND mpc.e <= {e_max} AND mpc.q/(1.0-mpc.e) >= {a_min} ORDER by mpc.mpcDesignation"
+        
     return query
-
-# testing edits
-# test 2 -- will it be easier?
--- tests/test_query.py
+++ tests/test_query.py
@@ -1,16 +1,57 @@
 from sso_query.query import make_query
 
 class TestQuery:
-    def test_defaults(self):
-        expected_query = "foo"
+    def test_lpcs(self):
+        expected_query = f"""SELECT mpc.ssObjectId, mpc.mpcDesignation, mpc.e, mpc.q, mpc.incl FROM dp03_catalogs_10yr.MPCORB as mpc WHERE mpc.q/(1.0-mpc.e) >= 50.0 ORDER by mpc.mpcDesignation"""
 
-        query = make_query()
+        query = make_query(a_min=50.0)
 
         assert expected_query == query
 
-    def test_neos(self):
-        expected_query = "foo"
 
-        query = make_query(1.3)
+    def test_centaurs(self):
+        expected_query = f"""SELECT mpc.ssObjectId, mpc.mpcDesignation, mpc.e, mpc.q, mpc.incl FROM dp03_catalogs_10yr.MPCORB as mpc WHERE mpc.q/(1.0-mpc.e) >= 5.5 AND mpc.q/(1.0-mpc.e) <= 30.1 ORDER by mpc.mpcDesignation"""
+
+        query = make_query(a_min=5.5, a_max=30.1)
+
+        assert expected_query == query
+        
+
+    def test_tnos(self):
+        expected_query = f"""SELECT mpc.ssObjectId, mpc.mpcDesignation, mpc.e, mpc.q, mpc.incl FROM dp03_catalogs_10yr.MPCORB as mpc WHERE mpc.q/(1.0-mpc.e) >= 30.1 AND mpc.q/(1.0-mpc.e) <= 50.0 ORDER by mpc.mpcDesignation"""
+
+        query = make_query(a_min=30.1, a_max=50.0)
+
+        assert expected_query == query
+
+
+    def test_neptuniantrojans(self):
+        expected_query = f"""SELECT mpc.ssObjectId, mpc.mpcDesignation, mpc.e, mpc.q, mpc.incl FROM dp03_catalogs_10yr.MPCORB as mpc WHERE mpc.q/(1.0-mpc.e) >= 29.8 AND mpc.q/(1.0-mpc.e) <= 30.4 ORDER by mpc.mpcDesignation"""
+
+        query = make_query(a_min=29.8, a_max=30.4)
+
+        assert expected_query == query
+
+
+    def test_jupitertrojans(self):
+        expected_query = f"""SELECT mpc.ssObjectId, mpc.mpcDesignation, mpc.e, mpc.q, mpc.incl FROM dp03_catalogs_10yr.MPCORB as mpc WHERE mpc.e <= 0.3 AND mpc.q/(1.0-mpc.e) >= 4.8 AND mpc.q/(1.0-mpc.e) <= 5.4 ORDER by mpc.mpcDesignation"""
+
+        query = make_query(e_max=0.3, a_min=4.8, a_max=5.4)
+
+        assert expected_query == query
+
+        
+    def test_mbas(self):
+        expected_query = f"""SELECT mpc.ssObjectId, mpc.mpcDesignation, mpc.e, mpc.q, mpc.incl FROM dp03_catalogs_10yr.MPCORB as mpc WHERE mpc.q >= 1.66 AND mpc.q/(1.0-mpc.e) >= 2.0 AND mpc.q/(1.0-mpc.e) <= 3.2 ORDER by mpc.mpcDesignation"""
+
+        query = make_query(q_min=1.66, a_min=2.0, a_max=3.2)
+
+        assert expected_query == query
+
+    
+    def test_neos(self):
+        expected_query = f"""SELECT mpc.ssObjectId, mpc.mpcDesignation, mpc.e, mpc.q, mpc.incl FROM dp03_catalogs_10yr.MPCORB as mpc WHERE mpc.q <= 1.3 AND mpc.e <= 1.0 AND mpc.q/(1.0-mpc.e) >= 4.0 ORDER by mpc.mpcDesignation"""
+
+        query = make_query(q_max=1.3, e_max=1.0, a_min=4.0)
 
         assert expected_query == query
