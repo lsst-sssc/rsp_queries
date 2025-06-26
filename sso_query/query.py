9e08e9963c321d8039b28cb86c94f8ebb229f5ac
amineeva <amineeva@olin.edu>
2025-06-26 15:22:53 +0000

    Added docstring

--- sso_query/query.py
+++ sso_query/query.py
@@ -3,6 +3,19 @@
 # ordered in terms of specificity - narrow cases first
 
 def make_query(q_cutoff_min = None, q_cutoff = None, e_cutoff_min = None, e_cutoff = None, a_cutoff = None, a_cutoff_min = None): 
+    """
+    Function creates a string query that can be passed to SSOtap. 
+    Args:
+        q_cutoff_min: Float representing the minimum distance at perihelion, in au.
+        q_cutoff: Float representing the max distance at perihelion, in au. 
+        e_cutoff_min: Float representing the minimum orbital eccentricity.
+        e_cutoff: Float representing the max orbital eccentricity. 
+        a_cutoff_min: Float representing the minimum semi-major axis of the orbit, in au.
+        a_cutoff: Float representing the max semi-major axis of the orbit, in au.
+    Returns:
+        query: String representing query that can be passed to SSOtap.
+        
+    """
     query_start = f"""SELECT q/(1-e) as a, q, e FROM dp03_catalogs_10yr.MPCORB as mpc
             WHERE"""
     conditions = []
@@ -24,5 +37,7 @@ def make_query(q_cutoff_min = None, q_cutoff = None, e_cutoff_min = None, e_cuto
     query = query + ";"
 
     return query
+
+def run_query(query_string)
 
 
