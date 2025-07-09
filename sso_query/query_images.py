bfad96e9db7ac7b7f420fdb760a7b54835e06b90
Tim Lister <tlister@lco.global>
2025-07-08 20:42:14 -0700

    Chain fixtures so those tests now pass. monkeypatch out the ACCESS_TOKEN for the supposed-to-fail case

--- sso_query/query_images.py
+++ sso_query/query_images.py
@@ -1,38 +1,47 @@
 from astropy.coordinates import SkyCoord
 from lsst.rsp.utils import get_service_url, get_access_token
 
-def check_rsp_access():
+def check_rsp_access(dbg=False):
     """Check whether RSP access is going to work"""
 
     access_good = False
     url = get_service_url("tap")
     # If running inside the RSP or `EXTERNAL_INSTANCE_URL` is set in the environment
     # then the url should start with 'https://'
-    print("URL=",url)
+    if dbg: print("URL=",url)
     if url.startswith('https://'):
         access_good = True
         token = get_access_token()
-        print("Token=", token)
+        if dbg: print("Token=", token)
         # Check for token set
         if token.startswith('gt-') is False:
             access_good = False
     return access_good
 
 def build_query(center: SkyCoord, bands: list = ['g', 'r', 'i'], calib_level=2) -> str:
-    """Build the query for the ivoa.ObsCore
+    """Build the ADQL query to search ivoa.ObsCore for images
+
+    Args:
+        center (SkyCoord): Astropy SkyCoord of center to search
+        bands (list, optional): List of bands to search. Defaults to ['g', 'r', 'i'].
+        calib_level (int, optional): calibration level of images to return. Defaults to 2 (visitImage)
+
+    Returns:
+        str: _description_ADQL query string
+    """    """
     """
 
     query = "SELECT lsst_visit, lsst_detector, lsst_tract, lsst_patch, lsst_band," \
             "s_ra, s_dec, t_min, t_max, s_region\n" \
             "FROM ivoa.ObsCore\n"\
-            f"WHERE calib_level = {calib_level}"
-    bands_clause = "\n"
-    if bands and len(bands) > 0:
-        bands_clause = " OR ".join([f"lsst_band = '{band}'" for band in bands])
-        bands_clause = f"\nAND ({bands_clause})\n"
-    query += bands_clause
+            f"WHERE calib_level = {calib_level}\n"
     coordinate_clause = f"AND CONTAINS(POINT('ICRS', {center.ra.deg},{center.dec.deg}), s_region) = 1\n"
     query += coordinate_clause
+    bands_clause = ""
+    if bands and len(bands) > 0:
+        bands_clause = " OR ".join([f"lsst_band = '{band}'" for band in bands])
+        bands_clause = f"AND ({bands_clause})\n"
+    query += bands_clause
 
     return query
 
--- tests/test_query_images.py
+++ tests/test_query_images.py
@@ -24,6 +24,7 @@ def test_check_no_access(monkeypatch: pytest.MonkeyPatch) -> None:
     # Monkeypatch any potentially set environment variables (doesn't affect
     # the users"s environment)
     monkeypatch.setenv("EXTERNAL_INSTANCE_URL", "https://shout.at.cloud.com/")
+    monkeypatch.setenv("ACCESS_TOKEN", "")
     monkeypatch.setenv("TAP_ROUTE", "/api/tap")
 
     assert check_rsp_access() == False
@@ -34,29 +35,34 @@ def setup_base_query():
             "s_ra, s_dec, t_min, t_max, s_region\n" \
         "FROM ivoa.ObsCore\n" \
         "WHERE calib_level = 2\n" \
-        "AND (lsst_band = 'g' OR lsst_band = 'r' OR lsst_band = 'i')\n"\
         "AND CONTAINS(POINT('ICRS', 37.86,6.98), s_region) = 1\n"
 
-    yield expected_query
+    return expected_query
+
+@pytest.fixture
+def setup_band_query(setup_base_query):
+    expected_query = setup_base_query
+    expected_query += "AND (lsst_band = 'g' OR lsst_band = 'r' OR lsst_band = 'i')\n"
+    return expected_query
 
 @pytest.fixture
 def center():
-    yield SkyCoord(37.86, 6.98, unit='deg')
+    return SkyCoord(37.86, 6.98, unit='deg')
 
-def test_build_query(setup_base_query, center):
+def test_default_build_query(setup_band_query, center):
 
     query = build_query(center)
 
-    assert query == setup_base_query
+    assert query == setup_band_query
 
 def test_build_query_no_bands(setup_base_query, center):
 
     query = build_query(center, bands=None)
 
-    assert query == setup_base_query[0:setup_base_query.rfind('\n', 0, -2)]
+    assert query == setup_base_query
 
 def test_build_query_empty_bands(setup_base_query, center):
 
     query = build_query(center, bands=[])
 
-    assert query == setup_base_query[0:setup_base_query.rfind('\n', 0, -2)]
+    assert query == setup_base_query
